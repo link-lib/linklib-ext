1e155c7e3f5c3b4beabbb0076736b2f4fbf8f029
JesseLiii <jesseli751@gmail.com>
2024-04-09 15:12:12 -0700

    fix: change compiler from swc to react-plugin, working hot reload

---
+++ src/scripts/highlighter/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<p>Hello, World!</p>
+				<p>I'm a Chrome Extension Content Script!</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
