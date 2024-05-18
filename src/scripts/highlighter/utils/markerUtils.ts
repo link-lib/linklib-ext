b6ecc030b284d79601e16083b558fe528538325e
JesseLiii <jesseli751@gmail.com>
2024-05-18 14:55:46 -0700

    feat: clean up scroll height account

--- src/scripts/highlighter/utils/markerUtils.ts
+++ src/scripts/highlighter/utils/markerUtils.ts
@@ -2,19 +2,17 @@ export const getSelectedText = () => window.getSelection()?.toString() ?? '';
 
 export const getMarkerPosition = () => {
 	const selection = window.getSelection();
-	if (!selection || selection.rangeCount === 0) {
-		return { display: 'none' };
-	}
+	if (!selection || selection.rangeCount === 0) return {}; // Early return if selection is null or no range is selected
 
-	const range = selection.getRangeAt(0);
-	const rect = range.getBoundingClientRect();
 	const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 	const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
 
+	const rangeBounds = selection.getRangeAt(0).getBoundingClientRect();
 	return {
-		top: rect.top + scrollTop,
-		left: rect.left + scrollLeft,
-		display: 'block',
+		left: scrollLeft + rangeBounds.left + rangeBounds.width / 2 - 80,
+		top: scrollTop + rangeBounds.top - 50,
+		// top: rangeBounds.bottom + 10,
+		display: 'flex',
 	};
 };
 
