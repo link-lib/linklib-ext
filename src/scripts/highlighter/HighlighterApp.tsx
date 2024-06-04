e8d598d73765f3022d888e87dabb4f9e27f8e439
JesseLiii <jesseli751@gmail.com>
2024-06-04 14:41:01 -0700

    refactor: move Highlight to its own component

--- src/scripts/highlighter/HighlighterApp.tsx
+++ src/scripts/highlighter/HighlighterApp.tsx
@@ -1,63 +1,14 @@
 import { useEffect, useState } from 'react';
 
-import {
-	Popover,
-	PopoverContent,
-	PopoverTrigger,
-} from '@/components/ui/popover';
 import ActionBar from '@/scripts/highlighter/components/ActionBar/ActionBar';
-import NotesModal from '@/scripts/highlighter/components/NotesModal';
 import {
 	MarkerPosition,
 	getMarkerPosition,
 	getSelectedText,
 } from '@/scripts/highlighter/utils/markerUtils';
 import { createRoot } from 'react-dom/client';
+import { Highlight } from '@/scripts/highlighter/components/Highlight';
 // import ReactDOM from 'react-dom';
-
-const Highlight = ({
-	children,
-	notesOpen = false,
-	highlightElement,
-	initialRating = 0,
-}: {
-	children: React.ReactNode;
-	notesOpen?: boolean;
-	highlightElement: HTMLElement | null;
-	initialRating?: number;
-}) => {
-	const [note, setNote] = useState<string>('');
-	const [isPopoverOpen, setIsPopoverOpen] = useState<boolean>(notesOpen);
-	const [rating, setRating] = useState<number>(initialRating);
-
-	return (
-		<Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
-			<PopoverTrigger asChild>
-				<span
-					className='bg-green-400 cursor-pointer hover:bg-yellow-300'
-					style={{}}
-					onClick={(e) => {
-						e.preventDefault();
-						e.stopPropagation();
-						setIsPopoverOpen(true);
-					}}
-				>
-					{children}
-				</span>
-			</PopoverTrigger>
-			<PopoverContent className='w-[550px]'>
-				<NotesModal
-					note={note}
-					setNote={setNote}
-					onClose={() => setIsPopoverOpen(false)}
-					highlightElement={highlightElement}
-					rating={rating}
-					setRating={setRating}
-				/>
-			</PopoverContent>
-		</Popover>
-	);
-};
 
 const HighlighterApp = () => {
 	const [markerPosition, setMarkerPosition] = useState<
---
+++ src/scripts/highlighter/components/Highlight.tsx
@@ -0,0 +1,51 @@
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from '@/components/ui/popover';
+import NotesModal from '@/scripts/highlighter/components/NotesModal';
+import { useState } from 'react';
+
+export const Highlight = ({
+	children,
+	notesOpen = false,
+	highlightElement,
+	initialRating = 0,
+}: {
+	children: React.ReactNode;
+	notesOpen?: boolean;
+	highlightElement: HTMLElement | null;
+	initialRating?: number;
+}) => {
+	const [note, setNote] = useState<string>('');
+	const [isPopoverOpen, setIsPopoverOpen] = useState<boolean>(notesOpen);
+	const [rating, setRating] = useState<number>(initialRating);
+
+	return (
+		<Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
+			<PopoverTrigger asChild>
+				<span
+					className='bg-green-400 cursor-pointer hover:bg-yellow-300'
+					style={{}}
+					onClick={(e) => {
+						e.preventDefault();
+						e.stopPropagation();
+						setIsPopoverOpen(true);
+					}}
+				>
+					{children}
+				</span>
+			</PopoverTrigger>
+			<PopoverContent className='w-[550px]'>
+				<NotesModal
+					note={note}
+					setNote={setNote}
+					onClose={() => setIsPopoverOpen(false)}
+					highlightElement={highlightElement}
+					rating={rating}
+					setRating={setRating}
+				/>
+			</PopoverContent>
+		</Popover>
+	);
+};
