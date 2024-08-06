45a472794fbb91a2d640192490b29cf87a8b22ae
JesseLiii <jesseli751@gmail.com>
2024-08-05 23:15:31 -0700

    feat: halfway to fixing all classes

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -100,7 +100,7 @@ const DialogDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
 	<DialogPrimitive.Description
 		ref={ref}
-		className={cn(':bb-text-sm text-muted-foreground', className)}
+		className={cn('bb-text-sm bb-text-muted-foreground', className)}
 		{...props}
 	/>
 ));
