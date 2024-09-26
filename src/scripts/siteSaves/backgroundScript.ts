8b8814079a239384c7ab1b89c69a6c945708c62b
Isabelle Ilyia <isabelle.ilyia@gmail.com>
2024-09-26 11:53:37 -0400

    save link

--- src/scripts/siteSaves/backgroundScript.ts
+++ src/scripts/siteSaves/backgroundScript.ts
@@ -1,3 +1,5 @@
+import { saveLink } from '@/backend/saveLink';
+
 chrome.runtime.onMessage.addListener((message) => {
 	if (message.action === 'saveTweet') {
 		fetch('https://your-backend.com/api/save', {
@@ -10,5 +12,24 @@ chrome.runtime.onMessage.addListener((message) => {
 			.then((response) => response.json())
 			.then((data) => console.log('Success:', data))
 			.catch((error) => console.error('Error:', error));
+	}
+});
+
+// For "save page" from extension popup
+chrome.runtime.onMessage.addListener(async (message, _sender, sendResponse) => {
+	if (message.action === 'saveLinkPopup') {
+		chrome.tabs.query({ active: true }, function (tabs) {
+			console.log(tabs[0]);
+			const currTab = tabs[0];
+			// TODO: can't access the author and publish date for now because we don't have access to the document
+			saveLink({
+				url: currTab.url,
+				favicon: currTab.favIconUrl,
+				title: currTab.title,
+				author: undefined,
+				publishDate: undefined,
+				savedDate: new Date().toISOString(),
+			}).then(sendResponse);
+		});
 	}
 });
